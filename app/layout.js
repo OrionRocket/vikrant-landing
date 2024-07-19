530cb8dd00562f1ec655986ec0dcd4d3d5a578e3
OrionROcket <orion@pariltech.com>
2024-07-19 17:44:56 +0400

    Added Pixel

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -9,9 +10,40 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    // Create script element
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
+    script.onload = () => {
+      // Initialize Facebook Pixel
+      window.fbq('init', '412348771820889');
+      window.fbq('track', 'PageView');
+    };
+
+    // Insert the script into the DOM
+    document.head.appendChild(script);
+
+    // Clean up the script when the component unmounts
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style={{ display: 'none' }}
+            src='https://www.facebook.com/tr?id=412348771820889&ev=PageView&noscript=1'
+            alt='Facebook Pixel'
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
