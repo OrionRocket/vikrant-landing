9cb94a3c19ad19e956b532b4d98b57abd4612d21
OrionROcket <orion@pariltech.com>
2024-07-20 17:05:29 +0400

    Added Pixel

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,50 @@
-'use client';
 import { useEffect } from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: 'Insider Vikrant',
+  description: 'Join Now for IPL match pass and World Cup updates',
+};
+
 export default function RootLayout({ children }) {
   useEffect(() => {
-    // Create script element
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-    script.onload = () => {
-      // Initialize Facebook Pixel
+    // Function to load Facebook Pixel
+    const loadFacebookPixel = () => {
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        'script',
+        'https://connect.facebook.net/en_US/fbevents.js'
+      );
+
       window.fbq('init', '412348771820889');
       window.fbq('track', 'PageView');
     };
 
-    // Insert the script into the DOM
-    document.head.appendChild(script);
-
-    // Clean up the script when the component unmounts
-    return () => {
-      document.head.removeChild(script);
-    };
+    // Load the script only if it hasn't been loaded yet
+    if (!window.fbq) {
+      loadFacebookPixel();
+    }
   }, []);
 
   return (
